2beb7fb9d02eb45d5b2fd3b86d5d8bac0f10d3ba
leaf <tthd@163.com>
2017-08-27 17:51:12 +0800

    pgnotify

--- src/PgNotify.php
+++ src/PgNotify.php
@@ -5,39 +5,38 @@ namespace Ty\Leaf;
 class PgNotify
 {
 
-	public function default($msg = '')
+	public function default($message = '', $style = '', $position = '', $type = '', $timeout = 1)
 	{
-		
+		$option = [
+			'style' 	=> $style?: 'bar',
+	        'message' 	=> $message?:'',
+	        'position' 	=> $position?:'top',
+	        'type' 		=> $type?:'default',
+	        'timeout' 	=> $timeout?:4000,
+		];
+
+		return json_encode($option);
 	}
 
-	public function primary($msg = '')
+	public function info($message = '', $style = '', $position = '', $type = 'info', $timeout = 4000)
 	{
-
+		return $this->default($message, $style, $position, $type, $timeout);
 	}
 
-	public function complete($msg = '')
-	{
 
+	public function success($message = '', $style = '', $position = '', $type = 'success', $timeout = 4000)
+	{
+		return $this->default($message, $style, $position, $type, $timeout);
 	}
 
-	public function success($msg = '')
+	public function danger($message = '', $style = '', $position = '', $type = 'danger', $timeout = 4000)
 	{
-
+		return $this->default($message, $style, $position, $type, $timeout);
 	}
 
-	public function danger($msg = '')
+	public function warning($message = '', $style = '', $position = '', $type = 'warning', $timeout = 4000)
 	{
-
-	}
-
-	public function warning($msg = '')
-	{
-
-	}
-
-	public function white($msg = '')
-	{
-
+		return $this->default($message, $style, $position, $type, $timeout);
 	}
 
 }
