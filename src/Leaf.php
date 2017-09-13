54e37f75e8f87a29716d1eded0a3fae6446d5e74
leaf <leaf@leaf.local>
2017-09-13 10:42:28 +0800

    init

--- src/Leaf.php
+++ src/Leaf.php
@@ -11,4 +11,23 @@ class Leaf
     {
         return new Content($callable);
     }
+
+    /**
+     * get title
+     * @return [type] [description]
+     */
+    public function title()
+    {
+        return config('admin.title');
+    }
+
+    public static function css()
+    {
+
+    }
+
+    public static function js()
+    {
+
+    }
 }
